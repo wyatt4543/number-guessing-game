51af18c7dd802d1d4e3531336601dd13b2f7a573
wyatt4543 <93059435+wyatt4543@users.noreply.github.com>
2026-04-21 12:08:48 -0400

    feat: get the user's username

--- number_guess.sh
+++ number_guess.sh
@@ -2,3 +2,7 @@
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 SECRET_NUMBER=$((1 + RANDOM % 1000))
+
+# get the user's username
+echo Enter your username:
+read USERNAME
