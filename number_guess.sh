4335bccd79fa33d73f18420e69e66c19487006cc
wyatt4543 <93059435+wyatt4543@users.noreply.github.com>
2026-04-21 12:04:51 -0400

    fix: correct the database access variable

--- number_guess.sh
+++ number_guess.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 SECRET_NUMBER=$((1 + RANDOM % 1000))
