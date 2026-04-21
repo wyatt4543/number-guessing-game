00ee9bf68df79ff3ab3510de181896f61af311a7
wyatt4543 <93059435+wyatt4543@users.noreply.github.com>
2026-04-21 12:31:22 -0400

    feat: get the user's guess for the number

--- number_guess.sh
+++ number_guess.sh
@@ -33,5 +33,6 @@ else
   echo Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses.
 fi
 
-
-
+# ask for the user to guess the number
+echo Guess the secret number between 1 and 1000:
+READ USER_GUESS
