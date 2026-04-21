1303fad77979ba0792a5342ed470846d8c0a240d
wyatt4543 <93059435+wyatt4543@users.noreply.github.com>
2026-04-21 12:25:37 -0400

    feat: check for if the user already exists or if a new database entry needs to be made & give different welcome messages

--- number_guess.sh
+++ number_guess.sh
@@ -5,4 +5,33 @@ SECRET_NUMBER=$((1 + RANDOM % 1000))
 
 # get the user's username
 echo Enter your username:
-read USERNAME
+read USER
+
+# get the user's username from the database
+USERNAME=$($PSQL "SELECT username FROM user_information WHERE username='$USER'")
+
+# if the user does not exist
+if [[ -z $USERNAME ]]
+then
+  # set the username variable to the new value
+  USERNAME=$USER
+
+  # insert the user's username into the database
+  echo "$($PSQL "INSERT INTO user_information(username) VALUES('$USERNAME')")"
+
+  # print the welcome message
+  echo Welcome, $USERNAME! It looks like this is your first time here.
+else
+  # if the user exists
+  # find the number of games the user has played
+  GAMES_PLAYED=$($PSQL "SELECT games_played FROM user_information WHERE username='$USERNAME'")
+
+  # find the user's best game
+  BEST_GAME=$($PSQL "SELECT best_game FROM user_information WHERE username='$USERNAME'")
+
+  # print the welcome message
+  echo Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses.
+fi
+
+
+
